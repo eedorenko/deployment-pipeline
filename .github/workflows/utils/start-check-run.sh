a812ce8f3835b12e38d0f199945357ed5404746c
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-01-24 17:54:54 -0800

    update

--- .github/workflows/utils/start-check-run.sh
+++ .github/workflows/utils/start-check-run.sh
@@ -6,9 +6,9 @@
 gh api -X POST -H "Accept: application/vnd.github+json" \
       -H "X-GitHub-Api-Version: 2022-11-28" \
       -f name='Super Check' \
-      -f head_sha='${{ github.event.pull_request.head.sha }}' \
+      -f head_sha='$COMMIT_ID' \
       -f status='in_progress' \
       -f 'output[title]=My Check Run Title' \
       -f 'output[summary]=A *fancy* summary' \
       -f 'output[text]=More detailed Markdown **text**' \
-      /repos/${{ github.repository }}/check-runs
+      /repos/$REPO/check-runs
