cbeff011e73fa3788ea96694d45e748accaf094e
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-01-27 15:40:07 -0800

    update

--- .github/workflows/utils/complete-check-run.sh
+++ .github/workflows/utils/complete-check-run.sh
@@ -4,11 +4,11 @@ PROJECT=$1
 VERSION=$2
 REPO=$3
 
-set -eo pipefail  # fail on error
+echo $PROJECT
+echo $VERSION
+echo $REPO
 
-gh api -X GET -H "Accept: application/vnd.github+json" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    /repos/$REPO/check-runs | jq -r '.check_runs[] | select(.status == "in_progress")'
+set -eo pipefail  # fail on error
 
 # get a list of active check runs and iterate over them
 gh api -X GET -H "Accept: application/vnd.github+json" \
@@ -25,9 +25,9 @@ gh api -X GET -H "Accept: application/vnd.github+json" \
     echo $check_run_id
 done
 
-# gh api -X GET -H "Accept: application/vnd.github+json" \
-#     -H "X-GitHub-Api-Version: 2022-11-28" \
-#     /repos/eedorenko/deployment-pipeline/check-runs
+gh api -X GET -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    /repos/eedorenko/deployment-pipeline/commits/tags/plant-chat-api/0.0.1-70/check-runs
     
     
 #      | jq -r '.check_runs[] | select(.status == "in_progress")'
