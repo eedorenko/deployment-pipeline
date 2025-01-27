a13f3657d8bc1d81ae9db39e005a48d47b4cf351
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-01-27 14:32:47 -0800

    update

---
+++ .github/workflows/utils/complete-check-run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+PROJECT=$1
+VERSION=$2
+REPO=$3
+
+# get a list of active check runs and iterate over them
+gh api -X GET -H "Accept: application/vnd.github+json" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    /repos/$REPO/check-runs | jq -r '.check_runs[] | select(.status == "in_progress") | .id' | while read -r check_run_id; do
+    # # update the check run with the new status
+    # gh api -X PATCH -H "Accept: application/vnd.github+json" \
+    #     -H "X-GitHub-Api-Version: 2022-11-28" \
+    #     -f name='Ensure version for '$PROJECT \
+    #     -f 'output[title]=Ensure Version '$VERSION' for '$PROJECT \
+    #     -f 'output[summary]='$VERSION \
+    #     -f conclusion='success' \
+    #     /repos/$REPO/check-runs/$check_run_id
+    echo $check_run_id
+done
