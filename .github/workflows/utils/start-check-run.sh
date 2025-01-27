02132ba995e6cfcc7d7172132e1dde929a2d90d7
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-01-27 12:37:06 -0800

    update

--- .github/workflows/utils/start-check-run.sh
+++ .github/workflows/utils/start-check-run.sh
@@ -16,31 +16,14 @@ for project in $(yq eval '.snapshot | keys | .[]' $SNAPSHOT_FILE); do
     echo $project
     echo $version
 
-    # gh api -X POST -H "Accept: application/vnd.github+json" \
-    #     -H "X-GitHub-Api-Version: 2022-11-28" \
-    #     -f name='Ensure version for '$project \
-    #     -f 'output[title]=Ensure Version '$version' for '$project \
-    #     -f 'output[summary]='$version \
-    #     -f head_sha=$COMMIT_ID \
-    #     -f status='in_progress' \
-    #     /repos/$REPO/check-runs
-    gh api -X PATCH -H "Accept: application/vnd.github+json" \
+    gh api -X POST -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        -f name='Ensure version for plant-chat-api' \
-        -f 'output[title]=Ensure Version 0.0.1-70 for plant-chat-api' \
-        -f 'output[summary]=0.0.1-70' \
-        -f head_sha=fe3b3b9c51ef0da7a9a185ba25a57492db3c4efe \
-        -f conclusion='success' \
-        /repos/eedorenko/deployment-pipeline/check-runs/36249093684
-
-    gh api -X PATCH -H "Accept: application/vnd.github+json" \
-        -H "X-GitHub-Api-Version: 2022-11-28" \
-        -f name='Ensure version for plant-chat-ui' \
-        -f 'output[title]=Ensure Version 0.0.1-36 for plant-chat-ui' \
-        -f 'output[summary]=0.0.1-36' \
-        -f head_sha=fe3b3b9c51ef0da7a9a185ba25a57492db3c4efe \
-        -f conclusion='success' \
-        /repos/eedorenko/deployment-pipeline/check-runs/36249094032
+        -f name='Ensure version for '$project \
+        -f 'output[title]=Ensure Version '$version' for '$project \
+        -f 'output[summary]='$version \
+        -f head_sha=$COMMIT_ID \
+        -f status='in_progress' \
+        /repos/$REPO/check-runs
 
 done
 
@@ -49,11 +32,29 @@ done
 # -f 'output[text]=More detailed Markdown **text**' \
 
 
-    gh api -X PATCH -H "Accept: application/vnd.github+json" \
-        -H "X-GitHub-Api-Version: 2022-11-28" \
-        -f name='Ensure version for plant-chat-api' \
-        -f 'output[title]=Ensure Version 0.0.1-70 for plant-chat-api' \
-        -f 'output[summary]=0.0.1-70' \
-        -f head_sha=fe3b3b9c51ef0da7a9a185ba25a57492db3c4efe \
-        -f conclusion='success' \
-        /repos/eedorenko/deployment-pipeline/check-runs/36249093684
+    # gh api -X PATCH -H "Accept: application/vnd.github+json" \
+    #     -H "X-GitHub-Api-Version: 2022-11-28" \
+    #     -f name='Ensure version for plant-chat-api' \
+    #     -f 'output[title]=Ensure Version 0.0.1-70 for plant-chat-api' \
+    #     -f 'output[summary]=0.0.1-70' \
+    #     -f head_sha=fe3b3b9c51ef0da7a9a185ba25a57492db3c4efe \
+    #     -f conclusion='success' \
+    #     /repos/eedorenko/deployment-pipeline/check-runs/36249093684
+
+    # gh api -X PATCH -H "Accept: application/vnd.github+json" \
+    #     -H "X-GitHub-Api-Version: 2022-11-28" \
+    #     -f name='Ensure version for plant-chat-api' \
+    #     -f 'output[title]=Ensure Version 0.0.1-70 for plant-chat-api' \
+    #     -f 'output[summary]=0.0.1-70' \
+    #     -f head_sha=fe3b3b9c51ef0da7a9a185ba25a57492db3c4efe \
+    #     -f conclusion='success' \
+    #     /repos/eedorenko/deployment-pipeline/check-runs/36249093684
+
+    # gh api -X PATCH -H "Accept: application/vnd.github+json" \
+    #     -H "X-GitHub-Api-Version: 2022-11-28" \
+    #     -f name='Ensure version for plant-chat-ui' \
+    #     -f 'output[title]=Ensure Version 0.0.1-36 for plant-chat-ui' \
+    #     -f 'output[summary]=0.0.1-36' \
+    #     -f head_sha=fe3b3b9c51ef0da7a9a185ba25a57492db3c4efe \
+    #     -f conclusion='success' \
+    #     /repos/eedorenko/deployment-pipeline/check-runs/36249094032
