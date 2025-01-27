4458f20dc5040ab245c28931081feaaa1b11e4cc
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-01-27 15:07:35 -0800

    update

--- .github/workflows/utils/complete-check-run.sh
+++ .github/workflows/utils/complete-check-run.sh
@@ -4,6 +4,8 @@ PROJECT=$1
 VERSION=$2
 REPO=$3
 
+set -eo pipefail  # fail on error
+
 gh api -X GET -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     /repos/$REPO/check-runs | jq -r '.check_runs[] | select(.status == "in_progress")'
@@ -11,7 +13,7 @@ gh api -X GET -H "Accept: application/vnd.github+json" \
 # get a list of active check runs and iterate over them
 gh api -X GET -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    /repos/$REPO/check-runs | jq -r '.check_runs[] | select(.status == "in_progress") | .id' | while read -r check_run_id; do
+    /repos/$REPO/commits/tags/$PROJECT/$VERSION/check-runs | jq -r '.check_runs[] | select(.status == "in_progress") | .id' | while read -r check_run_id; do
     # # update the check run with the new status
     # gh api -X PATCH -H "Accept: application/vnd.github+json" \
     #     -H "X-GitHub-Api-Version: 2022-11-28" \
@@ -22,3 +24,11 @@ gh api -X GET -H "Accept: application/vnd.github+json" \
     #     /repos/$REPO/check-runs/$check_run_id
     echo $check_run_id
 done
+
+# gh api -X GET -H "Accept: application/vnd.github+json" \
+#     -H "X-GitHub-Api-Version: 2022-11-28" \
+#     /repos/eedorenko/deployment-pipeline/check-runs
+    
+    
+#      | jq -r '.check_runs[] | select(.status == "in_progress")'
+
