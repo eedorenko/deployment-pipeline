e9914b8be1c52ca5ca97ae46a163cba7017f32a1
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-01-27 12:21:31 -0800

    test

--- .github/workflows/utils/start-check-run.sh
+++ .github/workflows/utils/start-check-run.sh
@@ -18,12 +18,13 @@ for project in $(yq eval '.snapshot | keys | .[]' $SNAPSHOT_FILE); do
 
     gh api -X POST -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        -f name='Ensure Version for '$project \
+        -f name='Ensure version for '$project \
+        -f 'output[title]=Ensure Version '$version' for '$project \
         -f head_sha=$COMMIT_ID \
         -f status='in_progress' \
         /repos/$REPO/check-runs
 done
 
-# -f "output[title]=Ensure Version $version for $project" \
+
 # -f 'output[summary]=A *fancy* summary' \
 # -f 'output[text]=More detailed Markdown **text**' \
