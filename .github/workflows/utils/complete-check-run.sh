2c8580a8ca03b9856e13a137133f7bf19ac69324
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-01-27 16:02:55 -0800

    update

--- .github/workflows/utils/complete-check-run.sh
+++ .github/workflows/utils/complete-check-run.sh
@@ -13,7 +13,7 @@ set -eo pipefail  # fail on error
 # get a list of active check runs and iterate over them
 gh api -X GET -H "Accept: application/vnd.github+json" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    /repos/$REPO/commits/tags/$PROJECT/$VERSION/check-runs | jq -r ".check_runs[] | select(.output.summary == $PROJECT/$VERSION) | .id" | while read -r check_run_id; do
+    /repos/$REPO/commits/tags/$PROJECT/$VERSION/check-runs | jq -r ".check_runs[] | select(.output.summary == \"$PROJECT/$VERSION\") | .id" | while read -r check_run_id; do
     # update the check run with the new status
     gh api -X PATCH -H "Accept: application/vnd.github+json" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
